624b20e6274dc0e5bea968e38590cc9d75d99598
putrialuthfi <putrialuthfi@gmail.com>
2024-09-23 15:16:26 +0700

    added file Article

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './component/Home'
 import AboutUs from './component/AboutUs';
 import IniNavbar from './component/Navbar';
+import Article from './component/Article';
 import Footer from './component/Footer';
+
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/IniNavbar" element={<IniNavbar />} />
         <Route path="/Footer" element={<Footer />} />
+
+        <Route path="/Article" element={<Article />} />
       </Routes>
     </Router>
   );
