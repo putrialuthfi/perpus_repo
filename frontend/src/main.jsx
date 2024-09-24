44d207fdf3ffcb4fa21123f478525d270bc073d5
putrialuthfi <putrialuthfi@gmail.com>
2024-09-24 15:10:49 +0700

    modified file styling and file main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Navbar from './component/Navbar.jsx'
+import Beranda from './component/homepage.jsx'
+import AboutUs from './component/aboutus.jsx'
 import Footer from './component/footer.jsx'
+import CTA from './component/cta.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar />
-    <App />
-    <Footer />
+    {/* <Navbar /> */}
+    {/* <Beranda /> */}
+    {/* <App /> */}
+    {/* <Footer /> */}
+    {/* <AboutUs /> */}
+    <CTA />
 
   </React.StrictMode>,
 )
