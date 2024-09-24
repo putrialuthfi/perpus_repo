c1e147db03ca68551e85577155b034b089c231f3
putrialuthfi <putrialuthfi@gmail.com>
2024-09-24 10:20:15 +0700

    deleted All

--- README.md
+++
@@ -1,3 +0,0 @@
-# perpus_repo
-Membuat Sistem Informasi Perpustakaan
-Start: 23 Sept '24
--- frontend/src/component/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Home() {
-  return (
-    <img src="../img/logo-1.png" alt="" style={{width:'50px'}}/>
-  );
-}
-
-export default Home;
