cf3a27a97ba1df2993cdf31e50fe292c1c9e1578
putrialuthfi <putrialuthfi@gmail.com>
2024-09-25 15:06:04 +0700

    update file main.jsx

--- frontend/src/component/homepage.jsx
+++ frontend/src/component/homepage.jsx
@@ -1,51 +1,17 @@
 import Navbar from "./Navbar"
+import Hero from "./hero-section"
+import Service from "./service-section"
+import CTA from "./cta-section"
 import Footer from "./footer"
 
 function Beranda() {
     return (
         <>
-        <div style={{ position: 'relative' }}>
-          <Navbar />
-          <div style={{
-            position: 'absolute', 
-            top: '50%', 
-            left: '50%', 
-            transform: 'translate(-50%, -50%)', 
-            color: 'white',
-          }}>
-            <h2 style={{
-                fontSize: '1.5rem',
-                fontWeight: '700'
-                }}>
-                Si Perpus <br></br>
-                Temukan Buku, Perluas Wawasan, <br></br>
-                Kapan Saja, Di Mana Saja
-            </h2>
-            <br />
-            <p>
-                Mencari dan meminjam buku jadi lebih mudah! 
-                Akses ribuan koleksi dengan cepat, periksa ketersediaan buku secara real-time, dan dapatkan rekomendasi bacaan yang sesuai dengan minatmu. 
-                Nikmati pengalaman membaca yang lebih praktis, <br />kapan pun kamu butuhkan.
-            </p>
-          </div>
-          {/* <p style={{
-              position: 'absolute', 
-              top: '50%', 
-              left: '50%', 
-              transform: 'translate(-50%, -50%)', 
-              color: 'white', 
-              fontSize: '.5rem'
-          }}
-          >
-            Mencari dan meminjam buku jadi lebih mudah! 
-            Akses ribuan koleksi dengan cepat, periksa ketersediaan buku secara real-time, dan dapatkan rekomendasi bacaan yang sesuai dengan minatmu. 
-            Nikmati pengalaman membaca yang lebih praktis, kapan pun kamu butuhkan.
-          </p> */}
-          
-        </div>
-        <Footer />
+            <Hero />
+            <Service />  
+            <CTA /> 
+            <Footer /> 
         </>
     )
 };
-
 export default Beranda
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Navbar from './component/Navbar.jsx'
+import Hero from './component/hero-section.jsx'
 import Beranda from './component/homepage.jsx'
 import AboutUs from './component/aboutus.jsx'
 import Footer from './component/footer.jsx'
-import CTA from './component/cta.jsx'
+import Service from './component/service-section.jsx'
+import LoginAdmin from './component/login-admin.jsx'
+import LoginPembaca from './component/login-pembaca.jsx'
+import Catalog2 from './component/catalog-2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Beranda />
     {/* <Navbar /> */}
-    {/* <Beranda /> */}
+    {/* <Service /> */}
     {/* <App /> */}
     {/* <Footer /> */}
-    {/* <AboutUs /> */}
-    <CTA />
+    {/* <LoginAdmin /> */}
+    {/* <LoginPembaca /> */}
+    {/* <Catalog2 /> */}
 
   </React.StrictMode>,
 )
