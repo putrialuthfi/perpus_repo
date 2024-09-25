b692b5551fe870c892acee1904964b280c3ade68
putrialuthfi <putrialuthfi@gmail.com>
2024-09-25 14:59:13 +0700

    added file catalog 2

---
+++ frontend/src/component/catalog-2.jsx
@@ -0,0 +1,40 @@
+// import Navbar from "./Navbar";
+// import Footer from "./footer";
+
+function Catalog2() {
+    return (
+        <>
+        {/* <Navbar /> */}
+        <div className="w-full h-screen flex flex-col md:flex-row">
+            <div className="w-full flex justify-center items-center md:w-2/4 p-4 border">
+                <img src="../src/assets/img/buku-1.jpg" alt="Cover Buku" className="h-4/5 w-[75%] object-cover" />
+            </div>
+                <div className="w-full md:w-2/3 px-8 text-left pt-20 mr-10">
+                    <h2 className="font-bold text-lg mb-4">Spesifikasi Buku</h2>
+                        <ul className="mb-6">
+                            <li>Judul: Kisah, Perjuangan & Inspirasi: B.J. Habibie</li>
+                            <li>Penulis: Weda S. Atma</li>
+                            <li>Penerbit: Checklist</li>
+                            <li>Terbit: 19 Oktober 2017</li>
+                            <li>Bahasa: Indonesia</li>
+                            <li>ISBN: 9786026763983</li>
+                        </ul>
+
+                        <h2 className="font-bold text-lg mb-4">Deskripsi Buku</h2>
+                        <h3 className="text-sm mb-4">
+                            Siapa yang tidak mengenal B.J. Habibie? Beliau merupakan salah satu putra terbaik yang pernah dimiliki negeri ini. Indonesia patut berbangga memiliki Prof. Dr. Ing. H. Bacharuddin Jusuf Habibie. Tokoh dengan sejuta prestasi ini tak hanya dikenal sebagai presiden ke-3 Republik Indonesia, tapi juga insinyur ternama yang karya dan pencapaiannya melambung hingga ke kancah dunia. Meski begitu, rupanya perjuangan beliau tidaklah mudah.
+                        </h3>
+                        <ul className="text-sm">
+                            <li>Seperti apakah sosok B.J. Habibie?</li>
+                            <li>Bagaimana masa kecil B.J. Habibie?</li>
+                            <li>Bagaimana perjuangan beliau ketika menginjakkan kaki pertama kali di negeri orang?</li>
+                            <li>Bagaimana awal mula kisah beliau dalam dunia politik?</li>
+                        </ul>
+                </div>
+        </div>
+        {/* <Footer /> */}
+        </>
+    );
+}
+
+export default Catalog2;
