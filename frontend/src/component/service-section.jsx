127fcbcfbd020993b209dfac0e6f1c77d8da4dbc
putrialuthfi <putrialuthfi@gmail.com>
2024-09-25 11:28:20 +0700

    modified service-section

--- frontend/src/component/service-section.jsx
+++ frontend/src/component/service-section.jsx
@@ -1,39 +1,3 @@
-// function Service (){
-//     return(
-//         <div className="w-full justify-center items-center gap-[114px] inline-flex py-6">
-//             <div className="w-60 h-[441.06px] relative">
-//                 <div className="w-21 h-[362px] px-[30px] pt-[135.89px] pb-[136.11px] left-0 top-[49.06px] absolute bg-[#bbcb8a] rounded-3xl shadow flex-col justify-center items-center inline-flex">
-//                     <div className="self-stretch grow shrink basis-0 flex-col justify-start items-start inline-flex">
-//                         <div className="self-stretch text-[#121212] text-[44px] font-bold font-['Source Sans Pro']">2448+</div>
-//                         <div className="self-stretch text-[#121212] text-2xl font-semibold font-['Source Sans Pro']">jumlah koleksi buku</div>
-//                     </div>
-//                 </div>
-//                 <img className="w-24 h-24 left-[72.97px] top-0 absolute origin-top-left rotate-[1.15deg]" src="../src/assets/img/emot-1.png" />
-//             </div>
-//             <div className="w-60 h-[441.06px] relative">
-//                 <div className="w-21 h-[362px] px-[30px] pt-[135.89px] pb-[136.11px] left-0 top-[49.06px] absolute bg-[#A9B8F7] rounded-3xl shadow flex-col justify-center items-center inline-flex">
-//                     <div className="self-stretch grow shrink basis-0 flex-col justify-start items-start inline-flex">
-//                         <div className="self-stretch text-[#121212] text-[44px] font-bold font-['Source Sans Pro']">1000+</div>
-//                         <div className="self-stretch text-[#121212] text-2xl font-semibold font-['Source Sans Pro']">peminjam buku dari dalam & luar daerah</div>
-//                     </div>
-//                 </div>
-//                 <img className="w-24 h-24 left-[72.97px] top-0 absolute origin-top-left rotate-[1.15deg]" src="../src/assets/img/emot-2.png" />
-//             </div>
-//             <div className="w-60 h-[441.06px] relative">
-//                 <div className="w-21 h-[362px] px-[30px] pt-[135.89px] pb-[136.11px] left-0 top-[49.06px] absolute bg-[#F7C4A9] rounded-3xl shadow flex-col justify-center items-center inline-flex">
-//                     <div className="self-stretch grow shrink basis-0 flex-col justify-start items-start inline-flex">
-//                         <div className="self-stretch text-[#121212] text-[44px] font-bold font-['Source Sans Pro']">10+</div>
-//                         <div className="self-stretch text-[#121212] text-2xl font-semibold font-['Source Sans Pro']">taman asri untuk baca di tempat</div>
-//                     </div>
-//                 </div>
-//                 <img className="w-24 h-24 left-[72.97px] top-0 absolute origin-top-left rotate-[1.15deg]" src="../src/assets/img/emot-3.png" />
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Service
-
 function Service() {
     return (
         <div className="w-full flex flex-wrap justify-center items-center gap-16 py-16">
