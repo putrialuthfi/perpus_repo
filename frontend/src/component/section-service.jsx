79b83f3b2d292820ed536a266123ccaa39694249
putrialuthfi <putrialuthfi@gmail.com>
2024-09-30 10:14:53 +0700

    modified section service

--- frontend/src/component/section-service.jsx
+++ frontend/src/component/section-service.jsx
@@ -3,24 +3,24 @@ function Service() {
         <div className="w-full flex flex-wrap justify-center items-center gap-16 py-16">
             <div className="w-60 h-auto relative flex flex-col items-center">
                 <div className="w-60 h-[362px] px-[30px] pt-[135.89px] pb-[136.11px] bg-[#bbcb8a] rounded-3xl shadow-lg backdrop-blur-md flex flex-col justify-center items-center">
-                    <div className="text-[#121212] text-[44px] font-bold font-['Source Sans Pro']">2448+</div>
-                    <div className="text-[#121212] text-2xl font-semibold font-['Source Sans Pro']">jumlah koleksi buku</div>
+                    <div className="text-[#121212] text-[34px] font-bold font-['Source Sans Pro']">2448+</div>
+                    <div className="text-[#121212] text-xl font-semibold font-['Source Sans Pro']">jumlah koleksi buku</div>
                 </div>
                 <img className="w-24 h-24 absolute -top-8" src="../src/assets/img/emot-1.png" />
             </div>
 
             <div className="w-60 h-auto relative flex flex-col items-center">
                 <div className="w-60 h-[362px] px-[30px] pt-[135.89px] pb-[136.11px] bg-[#A9B8F7] rounded-3xl shadow-lg backdrop-blur-md flex flex-col justify-center items-center">
-                    <div className="text-[#121212] text-[44px] font-bold font-['Source Sans Pro']">1000+</div>
-                    <div className="text-[#121212] text-2xl font-semibold font-['Source Sans Pro']">peminjam buku dari dalam & luar daerah</div>
+                    <div className="text-[#121212] text-[34px] font-bold font-['Source Sans Pro']">1000+</div>
+                    <div className="text-[#121212] text-xl font-semibold font-['Source Sans Pro']">peminjam buku dari dalam & luar daerah</div>
                 </div>
                 <img className="w-24 h-24 absolute -top-8" src="../src/assets/img/emot-2.png" />
             </div>
 
             <div className="w-60 h-auto relative flex flex-col items-center">
                 <div className="w-60 h-[362px] px-[30px] pt-[135.89px] pb-[136.11px] bg-[#F7C4A9] rounded-3xl shadow-lg backdrop-blur-md flex flex-col justify-center items-center">
-                    <div className="text-[#121212] text-[44px] font-bold font-['Source Sans Pro']">10+</div>
-                    <div className="text-[#121212] text-2xl font-semibold font-['Source Sans Pro']">taman asri untuk baca di tempat</div>
+                    <div className="text-[#121212] text-[34px] font-bold font-['Source Sans Pro']">10+</div>
+                    <div className="text-[#121212] text-xl font-semibold font-['Source Sans Pro']">taman asri untuk baca di tempat</div>
                 </div>
                 <img className="w-24 h-24 absolute -top-8" src="../src/assets/img/emot-3.png" />
             </div>
