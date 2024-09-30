e5a372d746f7808cfd2df715b9b2da7d60b65662
putrialuthfi <putrialuthfi@gmail.com>
2024-09-30 14:44:33 +0700

    added file section catalog

--- frontend/src/component/section-catalog.jsx
+++ frontend/src/component/section-catalog.jsx
@@ -1,33 +1,190 @@
 function SectionCatalog() {
   return (
-    <div className="grid grid-cols-5 gap-4 mt-6 px-8">
-        <div className="border rounded-2xl h-76">
-          <div className="relative h-56">
+    <div className="grid grid-cols-5 gap-4 mt-2 px-4">
+        <div className="border rounded-2xl h-26">
+          <div className="h-30">
             <img
-              src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+              src="../src/assets/img/buku-3.png"
               alt="card-image"
               className="object-cover w-full h-full rounded-t-2xl"
             />
           </div>
           <div className="p-4">
-            <div className="text-lg font-semibold text-blue-gray mb-2">
-              UI/UX Review Check
+            <div className="text-md font-bold ">
+              Pendidikan Agama Islam  
             </div>
-            <div>
-              The place is close to Barceloneta Beach and bus stop just 2 min
-              by walk and near to &quot;Naviglio&quot; where you can enjoy the
-              main night life in Barcelona.
+            <div className="text-sm">
+              Kemendikbud & Kemenag RI 2021
             </div>
-            <div className="pt-4 flex flex-items">
-              <button className="bg-[#27A37F] text-white hover:bg-blue-700 font-bold py-2 px-4 rounded">
-                Read More
+            <div className="pt-4 grid grid-cols-2 gap-3">
+              <button className="bg-[#F9F9F9] text-[#333C3F] font-bold py-2 px-4 rounded flex flex-items items-center border border-[#2A7C76] rounded-lg">
+                <img 
+                  src="../src/assets/img/icon-detail.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                  Detail
               </button>
-              <button className="bg-[#27A37F] text-white hover:bg-blue-700 font-bold py-2 px-4 rounded">
-                Read More
+              <button className="bg-[#27A37F] text-white font-bold py-2 px-4 rounded-lg flex flex-items items-center">
+                <img 
+                  src="../src/assets/img/icon-book.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                Pinjam
               </button>
             </div>
           </div>
         </div>
+
+{/* 2 */}
+<div className="border rounded-2xl h-26">
+          <div className="h-30">
+            <img
+              src="../src/assets/img/buku-3.png"
+              alt="card-image"
+              className="object-cover w-full h-full rounded-t-2xl"
+            />
+          </div>
+          <div className="p-4">
+            <div className="text-md font-bold ">
+              Pendidikan Agama Islam  
+            </div>
+            <div className="text-sm">
+              Kemendikbud & Kemenag RI 2021
+            </div>
+            <div className="pt-4 grid grid-cols-2 gap-3">
+              <button className="bg-[#F9F9F9] text-[#333C3F] font-bold py-2 px-4 rounded flex flex-items items-center border border-[#2A7C76] rounded-lg">
+                <img 
+                  src="../src/assets/img/icon-detail.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                  Detail
+              </button>
+              <button className="bg-[#27A37F] text-white font-bold py-2 px-4 rounded-lg flex flex-items items-center">
+                <img 
+                  src="../src/assets/img/icon-book.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                Pinjam
+              </button>
+            </div>
+          </div>
+        </div>
+
+{/* 3 */}
+<div className="border rounded-2xl h-26">
+          <div className="h-30">
+            <img
+              src="../src/assets/img/buku-3.png"
+              alt="card-image"
+              className="object-cover w-full h-full rounded-t-2xl"
+            />
+          </div>
+          <div className="p-4">
+            <div className="text-md font-bold ">
+              Pendidikan Agama Islam  
+            </div>
+            <div className="text-sm">
+              Kemendikbud & Kemenag RI 2021
+            </div>
+            <div className="pt-4 grid grid-cols-2 gap-3">
+              <button className="bg-[#F9F9F9] text-[#333C3F] font-bold py-2 px-4 rounded flex flex-items items-center border border-[#2A7C76] rounded-lg">
+                <img 
+                  src="../src/assets/img/icon-detail.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                  Detail
+              </button>
+              <button className="bg-[#27A37F] text-white font-bold py-2 px-4 rounded-lg flex flex-items items-center">
+                <img 
+                  src="../src/assets/img/icon-book.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                Pinjam
+              </button>
+            </div>
+          </div>
+        </div>
+
+{/* 4 */}
+<div className="border rounded-2xl h-26">
+          <div className="h-30">
+            <img
+              src="../src/assets/img/buku-3.png"
+              alt="card-image"
+              className="object-cover w-full h-full rounded-t-2xl"
+            />
+          </div>
+          <div className="p-4">
+            <div className="text-md font-bold ">
+              Pendidikan Agama Islam  
+            </div>
+            <div className="text-sm">
+              Kemendikbud & Kemenag RI 2021
+            </div>
+            <div className="pt-4 grid grid-cols-2 gap-3">
+              <button className="bg-[#F9F9F9] text-[#333C3F] font-bold py-2 px-4 rounded flex flex-items items-center border border-[#2A7C76] rounded-lg">
+                <img 
+                  src="../src/assets/img/icon-detail.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                  Detail
+              </button>
+              <button className="bg-[#27A37F] text-white font-bold py-2 px-4 rounded-lg flex flex-items items-center">
+                <img 
+                  src="../src/assets/img/icon-book.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                Pinjam
+              </button>
+            </div>
+          </div>
+        </div>
+        
+{/* 5 */}
+<div className="border rounded-2xl h-26">
+          <div className="h-30">
+            <img
+              src="../src/assets/img/buku-3.png"
+              alt="card-image"
+              className="object-cover w-full h-full rounded-t-2xl"
+            />
+          </div>
+          <div className="p-4">
+            <div className="text-md font-bold ">
+              Pendidikan Agama Islam  
+            </div>
+            <div className="text-sm">
+              Kemendikbud & Kemenag RI 2021
+            </div>
+            <div className="pt-4 grid grid-cols-2 gap-3">
+              <button className="bg-[#F9F9F9] text-[#333C3F] font-bold py-2 px-4 rounded flex flex-items items-center border border-[#2A7C76] rounded-lg">
+                <img 
+                  src="../src/assets/img/icon-detail.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                  Detail
+              </button>
+              <button className="bg-[#27A37F] text-white font-bold py-2 px-4 rounded-lg flex flex-items items-center">
+                <img 
+                  src="../src/assets/img/icon-book.png" 
+                  alt="CTA Image" 
+                  className="w-4 h-auto"
+                />
+                Pinjam
+              </button>
+            </div>
+          </div>
+        </div>
+
     </div>
   );
 }
