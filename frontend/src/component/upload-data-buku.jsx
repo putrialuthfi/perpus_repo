b794e437f4921bbe9579841b155985ecda06d651
putrialuthfi <putrialuthfi@gmail.com>
2024-10-28 11:29:44 +0700

    modified file upload data buku

--- frontend/src/component/upload-data-buku.jsx
+++ frontend/src/component/upload-data-buku.jsx
@@ -52,19 +52,19 @@ function UploadDataBuku() {
             <div>
             <form action="" className="space-y-4 pl-0 md:pl-10 text-left">
 {/* UPLOAD DATA */}                
-<div className="space-y-2">
+            <div className="space-y-2">
                 <label htmlFor="kategori" className="w-full md:w-2/4 text-sm font-medium">
                     Foto Buku
                 </label>
-<div class="flex items-center justify-center w-3/4 space-x-6">
-    <label for="dropzone-file" class="flex flex-col text-left w-1/3 h-30 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 ">
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-            </svg>
-            <p class="text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold items-center justify-center">Click to upload</span> </p>
-            <p className="mb-2 text-sm">or drag and drop</p>
-        </div>
+            <div class="flex items-center justify-center w-3/4 space-x-6">
+                <label for="dropzone-file" class="flex flex-col text-left w-1/3 h-30 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 ">
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p class="text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold items-center justify-center">Click to upload</span> </p>
+                        <p className="mb-2 text-sm">or drag and drop</p>
+            </div>
         <input id="dropzone-file" type="file" class="hidden" />
     </label>
     <label for="dropzone-file" class="flex flex-col text-left w-1/3 h-30 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500">
@@ -89,11 +89,6 @@ function UploadDataBuku() {
     </label>
 </div> 
 </div>
-
-
-
-
-                
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   <div className="space-y-2">
                     <label htmlFor="kode_buku" className="w-full md:w-2/4 text-sm font-medium">
