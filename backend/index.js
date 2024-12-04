11d2e6207ccf5630ab6af43ff68246380f42b8ee
putrialuthfi <putrialuthfi@gmail.com>
2024-12-04 20:58:14 +0700

    modified file index

--- backend/index.js
+++ backend/index.js
@@ -2,36 +2,44 @@ import express from "express";
 import cors from "cors";
 import mysql from 'mysql';
 import bodyParser from 'body-parser';
+import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
-dotenv.config();
+// import router from "./routes/PembacaRoute.js";
+import router from "./routes/ReaderRoute.js";
 // import PembacaRoute from "./routes/PembacaRoute.js";
-import AdminsRoute from "./routes/AdminsRoute.js";
+import ReaderRoute from "./routes/ReaderRoute.js";
+import AdminRoute from "./routes/AdminRoute.js";
 import PeminjamRoute from "./routes/PeminjamRoute.js"
 import UploadBukuRoute from "./routes/UploadBukuRoute.js"
 import RiwPeminjamanRoute from "./routes/RiwPeminjamanRoute.js";
 import AktivitasRoute from "./routes/AktivitasRoute.js"
 import BukuRoute from "./routes/BukuRoute.js"
 
+dotenv.config();
+
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors({ credentials: true, origin:'http://localhost:5000' }))
+app.use(cookieParser());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(router);
 // app.use('/', PembacaRoute);
-app.use('/', AdminsRoute);
+app.use('/', ReaderRoute);
+app.use('/', AdminRoute);
 app.use('/', PeminjamRoute);
 app.use('/', UploadBukuRoute);
 app.use('/', RiwPeminjamanRoute);
 app.use('/', AktivitasRoute);
 app.use('/', BukuRoute);
-app.use('tesadmin', AdminsRoute);
+// app.use('tesadmin', AdminsRoute);
 
 // CREATE CONNECTION DB
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "db_siperpus"
+    database: "salinan_db_siperpus"
 });
 
 db.connect((err) => {
@@ -43,31 +51,7 @@ db.connect((err) => {
 });
 
 // Untuk menjalankan server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-// DISINI
-// const express = require('express');
-// const cors = require('cors');
-// const adminRoutes = require('./routes/AdminsRoute');
-// import express from 'express';
-// import cors from 'cors';
-// import adminRoutes from './routes/AdminsRoute.js';
-
-
-// const app = express();
-// const PORT = 3000;
-
-// // Middleware
-// app.use(cors()); // Mengatasi masalah CORS
-// app.use(express.json()); // Untuk parsing JSON body
-
-// // Routes
-// app.use('/', adminRoutes);
-
-// // Start server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
