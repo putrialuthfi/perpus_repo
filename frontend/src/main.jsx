a056c47763de746594362880dab1fcb9a48d2a19
putrialuthfi <putrialuthfi@gmail.com>
2024-12-05 10:01:46 +0700

    modified file main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import {
   Card,
   CardHeader,
@@ -12,14 +12,15 @@ import {
 } from "@material-tailwind/react"
 import App from './App.jsx'
 import { ThemeProvider } from "@material-tailwind/react";
-import './index.css'
-import Navbar from './component/Navbar.jsx'
-import Hero from './component/section-hero.jsx'
-import Beranda from './component/homepage.jsx'
-import TentangKami from './component/tentang-kami.jsx'
-import Footer from './component/footer.jsx'
+// import { AuthProvider } from '../context/AuthContext.js';
+import './index.css';
+// import Navbar from './component/navbar.jsx';
+import Hero from './component/section-hero.jsx';
+import Beranda from './component/homepage.jsx';
+import TentangKami from './component/tentang-kami.jsx';
+import Footer from './component/footer.jsx';
 import LamanCatalog1 from './component/laman-catalog-1.jsx';
-import LamanCatalog2 from './component/laman-catalog-2.jsx'
+import LamanCatalog2 from './component/laman-catalog-2.jsx';
 import FormPinjamBuku from './component/form-pinjambuku.jsx';
 import Article from './component/article.jsx';
 import LoginAdmin from './component/login-admin.jsx';
@@ -36,28 +37,31 @@ import AktivitasSaya2 from './component/aktivitas-saya-2.jsx';
 
 const Home = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Beranda />} />
-        <Route path="/beranda" element={<Beranda />} />
-        <Route path="/loginadmin" element={<LoginAdmin />} />
-        <Route path="/loginpembaca" element={<LoginPembaca />} />
-        <Route path="/regisadmin" element={<RegisAdmin />} />
-        <Route path="/regispembaca" element={<RegisPembaca />} />
-        <Route path="/profiladmin" element={<ProfilAdmin />} />
-        <Route path="/profilpembaca" element={<ProfilPembaca />} />
-        <Route path="/datapeminjambuku" element={<DataPeminjamBuku />} />
-        <Route path="/uploaddatabuku" element={<UploadDataBuku />} />
-        <Route path="/riwayatpeminjaman" element={<RiwayatPeminjaman />} />
-        <Route path="/aktivitassaya" element={<AktivitasSaya />} />
-        <Route path="/LamanCatalog1" element={<LamanCatalog1 />} />
-        <Route path="/LamanCatalog2" element={<LamanCatalog2 />} />
-        <Route path="/FormPinjamBuku" element={<FormPinjamBuku />} />
-        <Route path="/tentangkami" element={<TentangKami />} />
-        <Route path="/Article" element={<Article />} />
-        {/* <Route path="/DataComponent" element={<DataComponent />} /> */}
-      </Routes>
-    </Router>
+    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Beranda />} />
+          <Route path="/beranda" element={<Beranda />} />
+          {/* <Route path="/navbar" element={<Navbar />} /> */}
+          <Route path="/loginadmin" element={<LoginAdmin />} />
+          <Route path="/loginpembaca" element={<LoginPembaca />} />
+          <Route path="/regisadmin" element={<RegisAdmin />} />
+          <Route path="/regispembaca" element={<RegisPembaca />} />
+          <Route path="/profiladmin" element={<ProfilAdmin />} />
+          <Route path="/profilpembaca" element={<ProfilPembaca />} />
+          <Route path="/datapeminjambuku" element={<DataPeminjamBuku />} />
+          <Route path="/uploaddatabuku" element={<UploadDataBuku />} />
+          <Route path="/riwayatpeminjaman" element={<RiwayatPeminjaman />} />
+          <Route path="/aktivitassaya" element={<AktivitasSaya />} />
+          <Route path="/LamanCatalog1" element={<LamanCatalog1 />} />
+          <Route path="/LamanCatalog2" element={<LamanCatalog2 />} />
+          <Route path="/FormPinjamBuku" element={<FormPinjamBuku />} />
+          <Route path="/tentangkami" element={<TentangKami />} />
+          <Route path="/Article" element={<Article />} />
+          {/* <Route path="/DataComponent" element={<DataComponent />} /> */}
+        </Routes>
+      </BrowserRouter>
+    
   )
 }
 
